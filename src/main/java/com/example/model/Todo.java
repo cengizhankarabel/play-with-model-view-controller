88fdfae8b17dca858522a91b4c2ac968788a02a7
CENGIZHAN KARABEL <cengizhankarabel@gmail.com>
2023-02-16 16:49:29 -0500

    getter/setter created

--- src/main/java/com/example/model/Todo.java
+++ src/main/java/com/example/model/Todo.java
@@ -2,5 +2,36 @@ package com.example.model;
 
 public class Todo {
 
-    // TODO: 2/15/2023  
+    private int id;
+    private String title;
+    private boolean completed;
+
+    public Todo(String title, boolean completed) {
+        this.title = title;
+        this.completed = completed;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
 }
