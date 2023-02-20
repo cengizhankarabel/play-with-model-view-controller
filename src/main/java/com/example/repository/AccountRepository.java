5ad1fa6e19c92b64614b74aec5a1bc43e59bf394
CENGIZHAN KARABEL <cengizhankarabel@gmail.com>
2023-02-20 11:14:55 -0500

    sss

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -1,6 +1,29 @@
 package com.example.repository;
 
+import com.example.model.Account;
+import com.example.model.Todo;
+
+import java.util.List;
+
 public interface AccountRepository {
+
+    // save acc
+    // update acc ( email )
+    // delete acc ( id )
+    // find acc number ( id )
+
+    void saveAccount(Account account);
+
+    String findMyAccountNumberByEmail(int accountId);
+
+    Account findMyAccount(int accountId);
+
+    void updateAccount(Account account);
+
+    void deleteAccount(int accountId);
+
+    List<Todo> getAllAccount ();
+
 
 
 }
