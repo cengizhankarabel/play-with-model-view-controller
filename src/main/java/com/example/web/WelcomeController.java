a25c4dbae91a3f2b4bdb01dd111b95585f672cfc
CENGIZHAN KARABEL <cengizhankarabel@gmail.com>
2023-02-20 18:38:10 -0500

    welcome controller initialized

--- src/main/java/com/example/web/WelcomeController.java
+++ src/main/java/com/example/web/WelcomeController.java
@@ -1,6 +1,19 @@
 package com.example.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class WelcomeController {
 
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = "/com/welcome"
+    )
+    public @ResponseBody String doWelcome(){
+        return "welcome to spring-web";
+
+    }
 }
