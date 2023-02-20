ae93a98638d3a424c998b6dc2c29b0cc2b2e73d6
CENGIZHAN KARABEL <cengizhankarabel@gmail.com>
2023-02-20 11:52:51 -0500

    db connection initialized

---
+++ src/main/java/com/example/db/DatabaseConnectionFactory.java
@@ -0,0 +1,42 @@
+package com.example.db;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DatabaseConnectionFactory {
+
+    private static Properties properties = new Properties();
+
+    static {
+
+        // load connection properties
+        try {
+            FileInputStream fis = new FileInputStream("src/main/resources/application.properties");
+            properties.load(fis);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // Register JDBC Driver
+        try {
+            Class.forName(properties.getProperty("db.driver"));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+
+        // Create DB Connection
+        String url = properties.getProperty("db.url");
+        String username = properties.getProperty("db.username");
+        String password = properties.getProperty("db.password");
+        return DriverManager.getConnection(url, username, password);
+    }
+}
